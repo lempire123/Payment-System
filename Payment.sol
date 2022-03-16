7182a67fb078025256f501ca2bae1584a4413d4d
lempire123 <61431140+lempire123@users.noreply.github.com>
2022-03-16 10:26:02 +0100

    Update Payment.sol

--- Payment.sol
+++ Payment.sol
@@ -5,6 +5,7 @@ contract Payment {
 
     // A position represents the relationship between the Owner and spender
     struct Position {
+        address owner;
         address spender;
         uint256 totalDeposit;
         uint256 totalClaimed;
@@ -13,12 +14,9 @@ contract Payment {
         bool created;
     }
 
+    uint256 counter = 0;
     // Array to keep track of all position ever created
     Position[] public positions;
-    
-    // Mapping of owner to its corresponding position 
-    // Each owner can only have one position (could be changed to have an array of positions)
-    mapping(address => Position) public ownerPosition;
 
     // Allows anyone to create a position
     function createPosition(
@@ -26,10 +24,9 @@ contract Payment {
         uint256 _weeklyAllowance
         ) public payable {
           
-        // Cannot create more than one position
-        require(ownerPosition[msg.sender].created == false, "Can only create one position");
         // Creation of position
         Position memory newPosition = Position(
+            msg.sender,
             _spender,
             msg.value,
             0,
@@ -38,18 +35,16 @@ contract Payment {
             true
         );
 
-        // Mapping is added
-        ownerPosition[msg.sender] = newPosition;
-
         // Position is pushed to the array
         positions.push(newPosition);
+        
     }
-    
+
     // Sends the claimable amount of a position to its corresponding spender
     function claimPositions() public {
 
         for(uint i; i < positions.length; i++) {
-            uint256 weeksSinceLastClaim = (block.timestamp - positions[i].lastClaim) / 1 weeks;
+            uint256 weeksSinceLastClaim = (block.timestamp - positions[i].lastClaim) / 10 seconds;
             uint256 claimable = weeksSinceLastClaim * positions[i].weeklyAllowance;
             // Make sure the position has enough funds
             require(positions[i].totalDeposit >= claimable, "Insufficient funds");
@@ -67,21 +62,21 @@ contract Payment {
     // ======= EDIT POSITION FUNCTIONS ============ // 
 
     // Allows the owner to change the allowance
-    function editAllowance(uint256 newAllowance) public {
-        Position storage position = ownerPosition[msg.sender];
-        position.weeklyAllowance = newAllowance;
+    function editAllowance(uint256 newAllowance, uint256 index) public {
+        require(positions[index].owner == msg.sender, "Cannot edit the position");
+        positions[index].weeklyAllowance = newAllowance;
     }
 
     // Allows the owner of a position to top up its balance
-    function deposit() public payable {
-        Position storage position = ownerPosition[msg.sender];
-        position.totalDeposit += msg.value;
+    function deposit(uint256 index) public payable {
+        positions[index].totalDeposit += msg.value;
     }
 
     // Allows the owner to withdraw ALL funds
-    function withdrawFunds() public {
-        Position storage position = ownerPosition[msg.sender];
-        uint256 bal = position.totalDeposit;
+    function withdrawFunds(uint256 index) public {
+        require(positions[index].owner == msg.sender, "Cannot edit the position");
+        uint256 bal = positions[index].totalDeposit;
+        positions[index].totalDeposit = 0;
         payable(msg.sender).transfer(bal);
     }
 }
